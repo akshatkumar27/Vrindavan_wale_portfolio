b0aae537f5847c87e9f6be2bbb974014b9d977b9
Akshat kumar <akshat@yokel.com>
2025-01-03 14:27:07 +0530

    map update

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,7 @@
 export function Map() {
   return (
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3559.0374341459437!2d80.92813237531791!3d26.776143176684714!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399bfb0062bff299%3A0x35be37f6ef3413dc!2sVrindavan%20wale%20Sweets%20and%20Namkeen!5e0!3m2!1sen!2sin!4v1710144547139!5m2!1sen!2sin"
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2995.305115582582!2d80.92928302483442!3d26.775053667460135!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399bfb0062bff299%3A0x35be37f6ef3413dc!2sVrindavan%20wale%20Sweets%20and%20Namkeen!5e0!3m2!1sen!2sin!4v1735894515628!5m2!1sen!2sin"
       className="w-full h-[400px] border-0"
       allowFullScreen
       loading="lazy"
